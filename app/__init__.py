153a164c6fb1e23c31286122154a7b45af2d3cda
gloriaodipo <gloriaodipo@gmail.com>
2018-07-25 14:34:11 +0300

    [CHORE] Import entry resource and add entry urls

--- app/__init__.py
+++ app/__init__.py
@@ -13,8 +13,10 @@ def create_app(config_name):
 
     with app.app_context():
         from app.resources.users import SignupResource, LoginResource
+        from app.resources.entries import EntryResource
 
     api.add_resource(SignupResource, '/api/v1/user/signup')
     api.add_resource(LoginResource, '/api/v1/user/login')
+    api.add_resource(EntryResource, '/api/v1/user/entries', '/api/v1/user/entries/<int:entry_id>')
 
     return app
