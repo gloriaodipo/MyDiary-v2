83b2627a9471b676d0953a4f757ade5d008fab74
gloriaodipo <gloriaodipo@gmail.com>
2018-08-02 18:26:04 +0300

    [FIX] Set url strict slashes to false

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,15 @@ from flask_cors import CORS
 
 import config
 
+
 def create_app(config_name):
     '''Function to create a flask app depending on the configuration passed'''
 
     app = Flask(__name__)
-    api=Api(app)
+    api = Api(app)
     CORS(app)
     app.config.from_object(config.app_config[config_name])
+    app.url_map.strict_slashes = False
 
     with app.app_context():
         from app.resources.users import SignupResource, LoginResource
@@ -18,7 +20,8 @@ def create_app(config_name):
 
     api.add_resource(SignupResource, '/api/v1/user/signup')
     api.add_resource(LoginResource, '/api/v1/user/login')
-    api.add_resource(EntryResource, '/api/v1/user/entries', '/api/v1/user/entries/<int:entry_id>')
+    api.add_resource(EntryResource, '/api/v1/user/entries',
+                     '/api/v1/user/entries/<int:entry_id>')
 
     @app.route("/")
     def index():
